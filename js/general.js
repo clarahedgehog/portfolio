1efe9c57432f192c4751f239deefe59485691c7b
Clara Swahn <clara.swahn@gmail.com>
2026-01-17 17:10:11 +0100

    add external library

--- js/general.js
+++ js/general.js
@@ -6,3 +6,24 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+// Wrap every letter in a span
+let textWrappers = Array.from(document.querySelectorAll('.intro-header'));
+
+textWrappers.forEach(textWrapper => {
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({ loop: false })
+    .add({
+      targets: '.intro-header .letter',
+      scale: [4, 1],
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 950,
+      delay: (el, i) => 70 * i
+    });
+})
+
+
